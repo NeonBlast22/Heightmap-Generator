1836e91ed72b80606fa00a524f6fc893b54dab9e
NeonBlast22 <wal@darthsmith.com>
2025-03-28 11:03:56 +1300

    Exporting To Downloads

--- Assets/Scripts/Exporter.cs
+++ Assets/Scripts/Exporter.cs
@@ -1,13 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using System;
+using TMPro;
 
 public class Exporter : MonoBehaviour
 {
-    [SerializeField] string path;
+    [SerializeField] TMP_Text outputLog;
+    string path;
+
+    private void Start()
+    {
+        string downloadsPath = "";
+        #if UNITY_ANDROID && !UNITY_EDITOR
+                downloadsPath = "/storage/emulated/0/Download/";
+        #elif UNITY_IOS && !UNITY_EDITOR
+                downloadsPath = Path.Combine(Application.persistentDataPath, "..", "Documents");
+        #else
+                downloadsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+        #endif
+        path = downloadsPath;
+    }
     public void Export(Texture2D texture)
     {
-        File.WriteAllBytes(path, texture.EncodeToPNG());
+        string date = DateTime.Now.ToShortDateString().Replace('/', '.');
+
+        string filePath = path + $"\\Export {date}.png";
+        if (File.Exists(filePath))
+        {
+            int i = 1;
+            while (File.Exists(path + $"\\Export {date} {i}.png"))
+            {
+                i++;
+                if (i > 10000)
+                {
+                    outputLog.text = $"Failed to Export: Why do you have over 1000 files in your downloads called Export {date}.png";
+                    return;
+                }
+            }
+            filePath = path + $"\\Export {date} {i}.png";
+        }
+        File.WriteAllBytes(filePath, texture.EncodeToPNG());
+        outputLog.text = $"Saved Export at {filePath}";
     }
 }
